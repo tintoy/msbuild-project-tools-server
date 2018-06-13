8f715dcb41ebd7e2f6cbe2e713d9d5273e7ee9d0
Adam Friedman <tintoy@tintoy.io>
2018-06-14 09:36:45 +1000

    Fix failing test (switched to netcoreapp2.0 version Microsoft.NET.Build.Tasks).

--- test/LanguageServer.Engine.Tests/TaskScannerTests.cs
+++ test/LanguageServer.Engine.Tests/TaskScannerTests.cs
@@ -31,6 +31,7 @@ namespace MSBuildProjectTools.LanguageServer.Tests
 
             TestOutput = testOutput;
         }
+
         /// <summary>
         ///     Output for the current test.
         /// </summary>
@@ -44,7 +45,7 @@ namespace MSBuildProjectTools.LanguageServer.Tests
         /// </param>
         [InlineData("NuGet.Build.Tasks.dll")]
         [InlineData("Microsoft.Build.Tasks.Core.dll")]
-        [InlineData("Sdks/Microsoft.NET.Sdk/tools/netcoreapp1.0/Microsoft.NET.Build.Tasks.dll")]
+        [InlineData("Sdks/Microsoft.NET.Sdk/tools/netcoreapp2.0/Microsoft.NET.Build.Tasks.dll")]
         [Theory(DisplayName = "TaskScanner can get tasks from framework task assembly ")]
         public async Task Scan_FrameworkTaskAssembly_Success(string fileName)
         {
