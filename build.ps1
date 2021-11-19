456b2ce68bbac54d7c6850cc43834b5f1a293697
Adam Friedman <tintoy@tintoy.io>
2021-11-20 09:26:32 +1100

    Target net6.0
    tintoy/msbuild-project-tools-vscode#88

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 ﻿$dotnet = Get-Command 'dotnet'
 
-& $dotnet publish "$PSScriptRoot\src\LanguageServer\LanguageServer.csproj" -f net5.0 -o "$PSScriptRoot\out\language-server"
-& $dotnet publish "$PSScriptRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -f net5.0 -o "$PSScriptRoot\out\task-reflection"
+& $dotnet publish "$PSScriptRoot\src\LanguageServer\LanguageServer.csproj" -f net6.0 -o "$PSScriptRoot\out\language-server"
+& $dotnet publish "$PSScriptRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -f net6.0 -o "$PSScriptRoot\out\task-reflection"
--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo 'Building language server...'
-dotnet publish src/LanguageServer/LanguageServer.csproj -f net5.0 -o $PWD/out/language-server
+dotnet publish src/LanguageServer/LanguageServer.csproj -f net6.0 -o $PWD/out/language-server
 
 echo 'Building task scanner...'
-dotnet publish src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj -f net5.0 -o $PWD/out/task-reflection
+dotnet publish src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj -f net6.0 -o $PWD/out/task-reflection
 
 echo 'Done.'
