25bc652ce9fe433e1b021748fd46880ea717be2d
DoctorKrolic <mapmyp03@gmail.com>
2023-06-28 19:42:45 +0300

    Centralize setting target framework and language version

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo 'Restoring Nuget packages...'
 dotnet restore
 
 echo 'Building language server...'
-dotnet publish src/LanguageServer/LanguageServer.csproj -f net6.0 -o $PWD/out/language-server
+dotnet publish src/LanguageServer/LanguageServer.csproj -o $PWD/out/language-server
 
 echo 'Building task scanner...'
-dotnet publish src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj -f net6.0 -o $PWD/out/task-reflection
+dotnet publish src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj -o $PWD/out/task-reflection
 
 echo 'Done.'
