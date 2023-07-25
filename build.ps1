a994920e25b631752f056a943a61cfb97775462a
Adam Friedman <tintoy@tintoy.io>
2023-07-25 23:30:35 +1000

    Remove all references to task-reflector project and its artifacts
    tintoy/msbuild-project-tools-server#42

--- build.ps1
+++ build.ps1
@@ -1,5 +1,4 @@
-﻿$dotnet = Get-Command 'dotnet'
+$dotnet = Get-Command 'dotnet'
 
 & $dotnet restore
 & $dotnet publish "$PSScriptRoot\src\LanguageServer\LanguageServer.csproj" -o "$PSScriptRoot\out\language-server"
-& $dotnet publish "$PSScriptRoot\src\LanguageServer.TaskReflection\LanguageServer.TaskReflection.csproj" -o "$PSScriptRoot\out\task-reflection"
--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ dotnet restore
 echo 'Building language server...'
 dotnet publish src/LanguageServer/LanguageServer.csproj -o $PWD/out/language-server
 
-echo 'Building task scanner...'
-dotnet publish src/LanguageServer.TaskReflection/LanguageServer.TaskReflection.csproj -o $PWD/out/task-reflection
-
 echo 'Done.'
