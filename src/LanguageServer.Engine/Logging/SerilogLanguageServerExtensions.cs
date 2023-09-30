bea189bfbb53ef0002abb344bf69886df542a647
Adam Friedman <tintoy@tintoy.io>
2023-09-30 18:35:02 +1000

    Include source component name in language-server log messages when log level is `Debug` or `Verbose`
    tintoy/msbuild-project-tools-server#71

--- src/LanguageServer.Engine/Logging/SerilogLanguageServerExtensions.cs
+++ src/LanguageServer.Engine/Logging/SerilogLanguageServerExtensions.cs
@@ -2,15 +2,88 @@ using OmniSharp.Extensions.LanguageServer.Server;
 using Serilog;
 using Serilog.Configuration;
 using Serilog.Core;
+using Serilog.Events;
 using System;
 
 namespace MSBuildProjectTools.LanguageServer.Logging
 {
     /// <summary>
-    ///     Extension methods for configuring Serilog.
+    ///     Extension methods for configuring and using Serilog.
     /// </summary>
     public static class SerilogLanguageServerExtensions
     {
+        /// <summary>
+        ///     Attempt to retrieve the log event's source component (i.e. last segment of the "SourceContext" property value).
+        /// </summary>
+        /// <param name="logEvent">
+        ///     The <see cref="LogEvent"/>.
+        /// </param>
+        /// <param name="sourceComponent">
+        ///     Receives the last segment (separator: '.') of the <see cref="LogEvent"/>'s "SourceContext" property value, if present.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c>, if the "SourceContext" property is present; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool TryGetSourceComponent(this LogEvent logEvent, out string sourceComponent)
+        {
+            if (logEvent == null)
+                throw new ArgumentNullException(nameof(logEvent));
+
+            sourceComponent = null;
+
+            string sourceContext;
+            if (!logEvent.TryGetSourceContext(out sourceContext))
+            {
+                sourceContext = "Unknown";
+
+                return false;
+            }
+
+            int lastSeparatorIndex = sourceContext.LastIndexOf('.');
+            if (lastSeparatorIndex != -1)
+                sourceComponent = sourceContext.Substring(lastSeparatorIndex + 1);
+            else
+                sourceComponent = sourceContext;
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Attempt to retrieve the log event's "SourceContext" (if present).
+        /// </summary>
+        /// <param name="logEvent">
+        ///     The <see cref="LogEvent"/>.
+        /// </param>
+        /// <param name="sourceContext">
+        ///     Receives the <see cref="LogEvent"/>'s "SourceContext" property value, if present.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c>, if the "SourceContext" property is present; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool TryGetSourceContext(this LogEvent logEvent, out string sourceContext)
+        {
+            if (logEvent == null)
+                throw new ArgumentNullException(nameof(logEvent));
+
+            sourceContext = null;
+
+            LogEventPropertyValue rawPropertyValue;
+            if (!logEvent.Properties.TryGetValue("SourceContext", out rawPropertyValue))
+                return false;
+
+            ScalarValue scalarPropertyValue = rawPropertyValue as ScalarValue;
+            if (scalarPropertyValue == null)
+                return false;
+
+            string propertyValue = scalarPropertyValue.Value as string;
+            if (String.IsNullOrWhiteSpace(propertyValue))
+                return false;
+
+            sourceContext = propertyValue;
+
+            return true;
+        }
+
         /// <summary>
         ///     Write log events to the language server logging facility.
         /// </summary>
