933a2cb0a628d4b5971920aa4567bbe11366296a
Andreas Böhm <aboehm@matherm.de>
2025-10-16 10:55:21 +0200

    Fix attributes for custom handler interfaces

--- src/LanguageServer.Engine/CustomProtocol/CustomCompletionHandler.cs
+++ src/LanguageServer.Engine/CustomProtocol/CustomCompletionHandler.cs
@@ -9,7 +9,7 @@ namespace MSBuildProjectTools.LanguageServer.CustomProtocol
     /// <summary>
     ///     Custom handler for "textDocument/completion" that accepts <see cref="CompletionParams"/> (the built-in version simply uses <see cref="TextDocumentPositionParams"/>, which doesn't include information about how completion was triggered).
     /// </summary>
-    [Method("textDocument/completion")]
+    [Parallel, Method("textDocument/completion")]
     public interface ICustomCompletionHandler
         : IRequestHandler<CompletionParams, CompletionList>, IJsonRpcHandler, IJsonRpcRequestHandler<CompletionParams, CompletionList>, IRegistration<CompletionRegistrationOptions>, ICapability<CompletionCapability>
     {
