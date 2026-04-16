49fdbe52078c090bad59cdafa635cb67b67b2ee2
Adam Friedman <tintoy@tintoy.io>
2026-04-16 11:42:40 +1000

    Simplify logic when determining applicable range for VsSSolutionException
    tintoy/msbuild-project-tools-server#136

--- src/LanguageServer.SemanticModel.VsSolutionXml/VsSolutionExceptionExtensions.cs
+++ src/LanguageServer.SemanticModel.VsSolutionXml/VsSolutionExceptionExtensions.cs
@@ -28,21 +28,15 @@ namespace MSBuildProjectTools.LanguageServer.SemanticModel
             var startPosition = new Position(
                 solutionException.Line.GetValueOrDefault(),
                 solutionException.Column.GetValueOrDefault()
-            );            
+            );
 
             // Attempt to use the range of the actual XML that the exception refers to.
             XmlLocation? location = xmlLocator?.Inspect(startPosition);
             if (location != null)
                 return location.Node.Range;
 
-            // Otherwise, fall back to using the exception's declared start position...
-            var endPosition = startPosition;
-
-            // ...although it's sometimes less reliable.
-            if (endPosition == Position.Zero)
-                endPosition = startPosition;
-
-            return new Range(startPosition, endPosition);
+            // Otherwise, just use the start position.
+            return startPosition.ToEmptyRange();
         }
     }
 }
